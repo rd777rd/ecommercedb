532db4c4cb9544dfc2abe6c1c3cc38072d7fb0ff
Romsites LLC <77874924+rd777rd@users.noreply.github.com>
2024-06-04 18:25:20 -0400

    Update server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(session({
 
 
 const db = mysql.createConnection({
-    host: "ecommercedb-lc0j.onrender.com",
+    host: "https://ecommercedb-q0qz.onrender.com",
     user:"c64e4582d0fe934ba8bca7ffbc2d853a",
     password:"4ac7477384ba191af5ea93cedd8b8f09",
     database: 'signup'
