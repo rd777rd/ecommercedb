fa42dda58100313dcfe375585571f94a4e015e9c
Romsites LLC <77874924+rd777rd@users.noreply.github.com>
2024-06-05 10:31:08 -0400

    Update server.js

--- server.js
+++ server.js
@@ -11,12 +11,7 @@ const port = process.env.PORT || 20072
 const app = express();
 app.use(express.json());
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://ecommerce-imta.onrender.com');
-    res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Credentials: true')
-    next();
-    });
+app.use(cors());
   
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
