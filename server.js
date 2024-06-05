0e4ccd4a3d23746ba5496f36a5af47f0d07484e2
Romsites LLC <77874924+rd777rd@users.noreply.github.com>
2024-06-05 11:02:26 -0400

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', ['https://ecommerce-imta.onrender.com', 'https://ecommercedb-q0qz.onrender.com']);
+    res.header('Access-Control-Allow-Origin', ['https://ecommerce-imta.onrender.com]);
     res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Credentials: true')
     next();
