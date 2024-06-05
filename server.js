30a1a41411ff8ce01d40570e6f13496bfed7f939
Romsites LLC <77874924+rd777rd@users.noreply.github.com>
2024-06-05 09:44:58 -0400

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Headers', 
                'Origin, X-Requested-With, Content-Type, Accept');
     next();
+    });
   
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
