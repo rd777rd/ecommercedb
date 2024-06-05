9994e8f7df93c18e3c0b15be391b6300b3fd7404
Romsites LLC <77874924+rd777rd@users.noreply.github.com>
2024-06-05 11:42:45 -0400

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.use(cors({origin: ["https://ecommerce-imta.onrender.com", "https://ecommercedb-q0qz.onrender.com"],
  methods: ["POST", "GET"],
- credentials:include,
+ credentials:true,
  'Access-Control-Allow-Credentials' : true
              }));
 
