b655844344e335944716e73f9a09e222040ac57f
Romsites LLC <77874924+rd777rd@users.noreply.github.com>
2024-06-04 23:17:03 -0400

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const saltRounds = 10;
 const port = process.env.PORT || 8081
 const app = express();
 app.use(express.json());
-app.use(cors({origin: ["https://ecommerce-imta.onrender.com/login"],
+app.use(cors({origin: ["https://ecommerce-imta.onrender.com"],
  methods: ["POST", "GET"],
   credentials:true}));
   
