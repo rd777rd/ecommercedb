c5e5a0deab5549561a014f812554b4bcbd109c56
Romsites LLC <77874924+rd777rd@users.noreply.github.com>
2024-06-05 09:54:53 -0400

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://ecommercedb-q0qz.onrender.com');
     res.header('Access-Control-Allow-Headers', 
                'Origin, X-Requested-With, Content-Type, Accept');
     next();
