846eafa66cbb2da81e14b56066d22cff5175e330
Romsites LLC <77874924+rd777rd@users.noreply.github.com>
2024-06-05 23:01:11 -0400

    Update server.js

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app.get("/", (req, res) => {
 })
 app.post('/signup', (req, res) => {
     const password = req.body.password
-    const sql = "INSERT INTO `userslog`(`firstName`, `lastName`, `birthDate`, `address`, `email`, `password`) VALUES (?)";
+    const sql = "INSERT INTO `userlog`(`firstName`, `lastName`, `birthDate`, `address`, `email`, `password`) VALUES (?)";
     bcrypt.hash(password.toString(), saltRounds, (err, hash) => {
         if (err) {
             return res.json({Error: "Error for password" });
