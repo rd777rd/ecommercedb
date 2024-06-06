e2f6036c5b4173d9dea275b256374fda384f3605
Romsites LLC <77874924+rd777rd@users.noreply.github.com>
2024-06-05 22:25:11 -0400

    Update server.js

--- server.js
+++ server.js
@@ -33,10 +33,10 @@ app.use(session({
 
 
 const db = mysql.createConnection({
-    const MYSQL_ADDON_HOST= "bfnwurlpig5e68hclczy-mysql.services.clever-cloud.com",
-    const MYSQL_ADDON_USER= "uy8jydz3g4ijvtyq",
-    const MYSQL_ADDON_PASSWORD = "b4p6tD5LRgkEVOHLxIp",
-    const MYSQL_ADDON_DB = "bfnwurlpig5e68hclczy",
+    MYSQL_ADDON_HOST: "bfnwurlpig5e68hclczy-mysql.services.clever-cloud.com",
+    MYSQL_ADDON_USER: "uy8jydz3g4ijvtyq",
+    MYSQL_ADDON_PASSWORD: "b4p6tD5LRgkEVOHLxIp",
+    MYSQL_ADDON_DB: "bfnwurlpig5e68hclczy",
 
 
     host: MYSQL_ADDON_HOST,
