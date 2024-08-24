babdf841178800c6be4444c053c189d15652c7b9
Romsites LLC <77874924+rd777rd@users.noreply.github.com>
2024-08-24 15:49:59 -0400

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 8080
 const app = express();
 app.use(express.json());
 
-app.use(cors(
+app.use(cors({
   origin: ["https://ecommerce-imta.onrender.com", "https://ecommercedb-q0qz.onrender.com"],
   methods: ["POST", "GET"],
   credentials:true}));
